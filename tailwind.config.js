79c879d6dfa32805187f2777d30b46c066a09f9d
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2024-12-04 00:54:11 +0100

    Describe what you added or changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,28 @@ module.exports = {
     extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
-        roboto : ["Roboto", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
       },
       colors: {
         primary: {
           DEFAULT: "#003B95",
         },
+        secondary: {
+          DEFAULT: "#006CE4",
+        },
+        dark: {
+          DEFAULT: "#171717",
+          100: "#595959",
+          200: "#e7e7e7",
+        },
       },
       backgroundImage: {
-        'heroImg': "url('/src/public/asset/hero-bg.jpeg')",
+        heroImg: "url('/src/public/asset/hero-bg.jpeg')",
+        lagos: "url('/src/public/asset/lagos.jpg')", 
+        ikeja: "url('/src/public/asset/ikeja.jpg')", 
+        lekki: "url('/src/public/asset/Lekki.jpg')", 
+        abuja: "url('/src/public/asset/Abuja.jpg')", 
+        accra: "url('/src/public/asset/accra.jpg')", 
       },
     },
   },
